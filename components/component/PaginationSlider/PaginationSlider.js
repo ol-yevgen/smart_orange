49b79069e5541c32ec44e7ed2c343d4b834be8d1
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-15 02:42:38 +0300

    deploy test

--- components/component/PaginationSlider/PaginationSlider.js
+++ components/component/PaginationSlider/PaginationSlider.js
@@ -16,48 +16,87 @@ export const PaginationSlider = ({ data }) => {
     const windowWidth = useResize()
 
     return (
-        windowWidth.isScreenLg
-            ? <div className='slider'>
-                <Swiper
-                    spaceBetween={50}
-                    pagination={{
-                        type: 'fraction',
-                    }}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <div className="mySwiper__content">
-                        <h1 className='mySwiper__title'>
-                            project <br />
-                            <span className='mySwiper__title-dec'>home</span>
-                        </h1>
-                        <SliderArrows />
+        <div className='slider'>
+            <Swiper
+                spaceBetween={50}
+                pagination={{
+                    type: 'fraction',
+                }}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <div className="mySwiper__content">
+                    <h1 className='mySwiper__title'>
+                        project <br />
+                        <span className='mySwiper__title-dec'>home</span>
+                    </h1>
+                    <SliderArrows />
 
-                    </div>
-                    {
-                        data.map(slide => {
-                            return (
-                                <SwiperSlide key={slide.id} className='mySwiper__slide'>
-                                    <div>
+                </div>
+                {
+                    data.map(slide => {
+                        return (
+                            <SwiperSlide key={slide.id} className='mySwiper__slide'>
+                                <div>
 
-                                    </div>
-                                    <Image src={slide.path} width={770} height={829} className='mySwiper__image' alt="time" />
-                                    <Button
-                                        title='взглянуть'
-                                        color='btn-white'
-                                        position='btn__position-left'
-                                        circleColor='light'
-                                        link='/'
-                                    />
-                                </SwiperSlide>
-                            )
-                        })
-                    }
-                </Swiper>
+                                </div>
+                                <Image src={slide.path} width={770} height={829} className='mySwiper__image' alt="time" />
+                                <Button
+                                    title='взглянуть'
+                                    color='btn-white'
+                                    position='btn__position-left'
+                                    circleColor='light'
+                                    link='/'
+                                />
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
 
-            </div>
+        </div>
+        // windowWidth.isScreenLg
+        //     ? <div className='slider'>
+        //         <Swiper
+        //             spaceBetween={50}
+        //             pagination={{
+        //                 type: 'fraction',
+        //             }}
+        //             modules={[Pagination, Navigation]}
+        //             className="mySwiper"
+        //         >
+        //             <div className="mySwiper__content">
+        //                 <h1 className='mySwiper__title'>
+        //                     project <br />
+        //                     <span className='mySwiper__title-dec'>home</span>
+        //                 </h1>
+        //                 <SliderArrows />
 
-            : <MobilePaginationSlider data={data} />
+        //             </div>
+        //             {
+        //                 data.map(slide => {
+        //                     return (
+        //                         <SwiperSlide key={slide.id} className='mySwiper__slide'>
+        //                             <div>
+
+        //                             </div>
+        //                             <Image src={slide.path} width={770} height={829} className='mySwiper__image' alt="time" />
+        //                             <Button
+        //                                 title='взглянуть'
+        //                                 color='btn-white'
+        //                                 position='btn__position-left'
+        //                                 circleColor='light'
+        //                                 link='/'
+        //                             />
+        //                         </SwiperSlide>
+        //                     )
+        //                 })
+        //             }
+        //         </Swiper>
+
+        //     </div>
+
+        //     : <MobilePaginationSlider data={data} />
 
     );
 }
